af257311fc86c07c1eef5d8c3ffa94f549c4f84f
Excel Bill <60159086+Kessyblaise1@users.noreply.github.com>
2022-07-18 22:18:08 +0100

    started game functionality

---
+++ src/gameData/LogoData.js
@@ -0,0 +1,17 @@
+const logos = [
+  {
+    companyName: 'Mitsubishi',
+    logo: './images/logos/Mitsubishi.webp',
+    options: ['Mitsubishi', 'Red Diamond', 'Three Diamond']
+  },
+  {
+    companyName: 'Microsoft',
+    logo: './images/logos/microsoft.png',
+    options: ['Windows', 'Microsoft', 'Squares']
+  },
+  {
+    companyName: 'Peloton',
+    logo: './images/logos/peloton.png',
+    options: ['Pintrest', 'Peloton', 'Pixar Bay']
+  }
+]
--- src/js/game.js
+++ src/js/game.js
@@ -1,23 +1,36 @@
+// ======================================================================={  NODE ELEMENTS  }=========================================================================
 const pauseButton = document.querySelector(".pause")
 const resumeButton = document.querySelector(".resume")
 const pauseScreen = document.querySelector(".pause_screen")
+let optionsContainer = document.querySelector(".selections")
 
 const counter = document.querySelector(".counter")
 let index = 3
 counter.textContent = index
 
+// ======================================================================={  COUNTDOWN TIMER  }=========================================================================
 let countdownTimer = setTimeout(() => {
   counter.textContent = index- 1
 }, 1000);
 
+
+
+// ======================================================================={  PAUSE FUNCTIONALITY  }=========================================================================
 const pause = () => {
   pauseScreen.style.display = "flex"
 }
 
-const resume = () => {
 
+
+// ======================================================================={  RESUME FUNCTIONALITY  }=========================================================================
+const resume = () => {
   pauseScreen.style.display = "none"
 }
 
 pauseButton.addEventListener("click", pause)
 resumeButton.addEventListener("click", resume)
+
+
+// ======================================================================={  NEXT LOGO  }=========================================================================
+let opt = logos[0].options
+console.log(opt)

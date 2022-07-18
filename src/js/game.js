04258a8edb0f25e75b1f7f2da2aa2cc305962592
Excel Bill <60159086+Kessyblaise1@users.noreply.github.com>
2022-07-18 08:44:23 +0100

    creating countdown

--- src/js/game.js
+++ src/js/game.js
@@ -2,14 +2,20 @@ const pauseButton = document.querySelector(".pause")
 const resumeButton = document.querySelector(".resume")
 const pauseScreen = document.querySelector(".pause_screen")
 
+const counter = document.querySelector(".counter")
+let index = 3
+counter.textContent = index
 
-
+let countdownTimer = setTimeout(() => {
+  counter.textContent = index- 1
+}, 1000);
 
 const pause = () => {
   pauseScreen.style.display = "flex"
 }
 
 const resume = () => {
+
   pauseScreen.style.display = "none"
 }
 

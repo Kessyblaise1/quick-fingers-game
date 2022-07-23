abb451e2527c4795bedec9ff3b60169b8b6c6eb0
Excel Bill <60159086+Kessyblaise1@users.noreply.github.com>
2022-07-23 17:13:45 +0100

    solved setTimeOut problem

--- src/js/game.js
+++ src/js/game.js
@@ -1,82 +1,88 @@
 // ======================================================================={  NODE ELEMENTS  }=========================================================================
-const pauseButton = document.querySelector(".pause")
-const resumeButton = document.querySelector(".resume")
-const pauseScreen = document.querySelector(".pause_screen")
-let optionsContainer = document.querySelector(".selections")
-const image = document.querySelector('.logo_image')
-let scoreContainer = document.querySelector(".score")
-const startButton = document.querySelector(".start")
-const welcome = document.querySelector('.start_screen')
-let resumeCounter = document.querySelector(".resume_counter")
+const pauseButton = document.querySelector(".pause");
+const resumeButton = document.querySelector(".resume");
+const pauseScreen = document.querySelector(".pause_screen");
+let optionsContainer = document.querySelector(".selections");
+const image = document.querySelector(".logo_image");
+let scoreContainer = document.querySelector(".score");
+const startButton = document.querySelector(".start");
+const welcome = document.querySelector(".start_screen");
+let resumeCounter = document.querySelector(".resume_counter");
 
-let currentScore = document.querySelector(".current_score span")
+let currentScore = document.querySelector(".current_score span");
 
-const counter = document.querySelector(".counter")
-let startValue = 3
-counter.textContent = startValue
+const counter = document.querySelector(".counter");
+let startValue = 3;
+counter.textContent = startValue;
 
 // ======================================================================={  COUNTDOWN TIMER  }=========================================================================
 
-function startTimer(index) {
-  resumeCounter.style.display = 'flex'
-  let countdownTimer = setInterval(() => {
-    counter.textContent = index -= 1
-    if(index <= 0) {
-      clearInterval(countdownTimer)
-      resumeCounter.style.display = 'none'
-    }
-  }, 1000);
-}
+
 
 // ======================================================================={  PAUSE FUNCTIONALITY  }=========================================================================
 const pause = () => {
-  pauseScreen.style.display = "flex"
-}
-
-
+  pauseScreen.style.display = "flex";
+};
 
 // ======================================================================={  RESUME FUNCTIONALITY  }=========================================================================
 const resume = () => {
   startTimer(4)
-  pauseScreen.style.display = "none"
-}
+  pauseScreen.style.display = "none";
+};
 
-pauseButton.addEventListener("click", pause)
-resumeButton.addEventListener("click", resume)
-
-
+pauseButton.addEventListener("click", pause);
+resumeButton.addEventListener("click", resume);
 
 // ======================================================================={  NEXT LOGO  }=========================================================================
 
-
 // ======================================================================={  A KEEPER }=========================================================================
-let score = 0
-scoreContainer.textContent = score
-let finalValue = logos.length
+let score = 0;
+scoreContainer.textContent = score;
+let finalValue = logos.length;
 
-let id = 0
+let id = 0;
 
 function renderGame(id) {
-  welcome.remove()
-  image.setAttribute('src', logos[id].logo)
-  image.setAttribute('alt', logos[id].companyName + " logo")
-  optionsContainer.innerHTML = logos[id].options.map(opt => `<button class="option btn">${opt}</button>`).join("")
-  scoreContainer.textContent = score
-  currentScore.textContent = score
+  welcome.remove();
+  image.setAttribute("src", logos[id].logo);
+  image.setAttribute("alt", logos[id].companyName + " logo");
+  optionsContainer.innerHTML = logos[id].options
+    .map((opt) => `<button class="option btn">${opt}</button>`)
+    .join("");
+  scoreContainer.textContent = score;
+  currentScore.textContent = score;
 }
 
 // renderGame(id)
 
 // SELECTING THE OPTIONS  ////////////////////////////////////////
-optionsContainer.addEventListener('click', function(e) {
-  e.target.textContent == logos[id].companyName ? score += 5 : console.log('not working')
-  e.target.matches('button') && id != finalValue -1 ? renderGame(id += 1) : null
-})
+optionsContainer.addEventListener("click", function (e) {
+  e.target.textContent == logos[id].companyName ? (score += 5) : console.log("not working");
+  e.target.matches("button") && id != finalValue - 1 ? renderGame((id += 1)) : null;
+});
 
 // ======================================================================={  OPTION CLICK / NEXT LOGO }=========================================================================
-startButton.addEventListener('click', startGame)
+startButton.addEventListener("click", startGame);
 
 function startGame() {
-  startTimer(3)
+  startTimer(3);
+  renderGame(id);
+}
+
+function startTimer(index) {
+  resumeCounter.style.display = "flex";
+  let countdownTimer = setInterval(() => {
+    counter.textContent = index -= 1;
+    if (index <= 0) {
+      clearInterval(countdownTimer);
+      resumeCounter.style.display = "none";
+    }
+    const options = document.querySelectorAll(".option");
+    options.forEach((opt) => opt.setAttribute("disabled", "true"));
+  }, 1000);
+  let timeOut = setTimeout(() => {
+    const options = document.querySelectorAll(".option");
+    options.forEach((opt) => opt.removeAttribute("disabled"));
+  }, 4000);
   renderGame(id)
 }
